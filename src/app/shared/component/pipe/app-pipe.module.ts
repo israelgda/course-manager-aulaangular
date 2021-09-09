e7ea4cbf2476e12eb93ea3c54a84be4a02f3b68f
Israel Gadelha <gdelha.israel@gmail.com>
2021-09-09 17:46:28 -0300

    Feat: Criação da pasta Shared e realocação dos components Star e Pipe
    Criada a pasta Shared para reformular o local e modularizar os componentes Star e Pipe

---
+++ src/app/shared/component/pipe/app-pipe.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { ReplacePipe } from "src/app/pipe/replace.pipe";
+
+@NgModule({
+    declarations: [
+        ReplacePipe
+    ],
+    exports: [
+        ReplacePipe
+    ]
+})
+export class AppPipeModule {
+
+}
